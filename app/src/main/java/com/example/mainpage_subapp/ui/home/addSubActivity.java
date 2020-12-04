d93efb98116fda1738e246236bc52c03555a1710
paplio <ani.12.28.98@gmail.com>
2020-12-04 20:05:44 +0530

    added room package, created database, need to work on repo to access db next

--- app/src/main/java/com/example/mainpage_subapp/ui/home/addSubActivity.java
+++ app/src/main/java/com/example/mainpage_subapp/ui/home/addSubActivity.java
@@ -3,6 +3,9 @@ package com.example.mainpage_subapp.ui.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ScrollView;
 
 import com.example.mainpage_subapp.R;
 
@@ -12,5 +15,21 @@ public class addSubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sub);
+        ScrollView scr=findViewById(R.id.memberlist);
+        LayoutInflater inflater = LayoutInflater.from(addSubActivity.this);
+        View inflatedLayout = inflater.inflate(R.layout.member_entry, null, false);
+        scr.addView(inflatedLayout);
+
+        LayoutInflater inflater1 = LayoutInflater.from(addSubActivity.this);
+        View inflatedLayout1 = inflater1.inflate(R.layout.member_entry, null, false);
+        scr.addView(inflatedLayout1);
+
+        LayoutInflater inflater2 = LayoutInflater.from(addSubActivity.this);
+        View inflatedLayout2 = inflater2.inflate(R.layout.member_entry, null, false);
+        scr.addView(inflatedLayout2);
+
+        LayoutInflater inflater3 = LayoutInflater.from(addSubActivity.this);
+        View inflatedLayout3 = inflater3.inflate(R.layout.member_entry, null, false);
+        scr.addView(inflatedLayout3);
     }
 }
