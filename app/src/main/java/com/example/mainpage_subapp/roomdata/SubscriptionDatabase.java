6700657885de096261468c64df552d73e4e032e7
paplio <ani.12.28.98@gmail.com>
2020-12-15 13:44:32 +0530

    updated db version

--- app/src/main/java/com/example/mainpage_subapp/roomdata/SubscriptionDatabase.java
+++ app/src/main/java/com/example/mainpage_subapp/roomdata/SubscriptionDatabase.java
@@ -20,21 +20,12 @@ import java.util.Date;
 
 
 
-@Database(entities = {Subscription.class, SubscriptionGroup.class}, version = 1)
+@Database(entities = {Subscription.class, SubscriptionGroup.class}, version = 2)
 public abstract class SubscriptionDatabase extends RoomDatabase {
 
-    //okay, buckle up because we are gonna be defining a buncha weird stuff here
-
-    //First, create an instance of the SubscriptionDatabase object called 'instance' by default
-    //this guy basically is used to access and control all things we do with the database
     private static SubscriptionDatabase instance;
 
-    //this is a method that allows us to access all of our Dao methods - insert, update, delete etc etc
     public abstract SubscriptionDao subscriptionDao();
-
-    //create a static, synchronised (not s the other letter!) SubscriptionDatabase function called getInstance
-    //this is used to interact with the private static instance object
-    //- it is synced so that we dont have two threads working on our database at the same time - kinda cool!
 
     public static synchronized SubscriptionDatabase getInstance(Context context) {
         //we only create an instance of our database if it doesn't already exist
